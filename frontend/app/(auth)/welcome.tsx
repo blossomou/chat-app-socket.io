2f0ef7904fb5d0aca72eabd2aee5e4614dd4e7d2
Qing Ou <qingqingou@gmail.com>
2026-05-05 10:14:38 -0400

    set up welcome screen (WIP)

--- frontend/app/(auth)/welcome.tsx
+++ frontend/app/(auth)/welcome.tsx
@@ -1,15 +1,60 @@
 import ScreenWrapper from '@/components/ScreenWrapper';
+import Typo from '@/components/Typo';
+import { colors, spacingX, spacingY } from '@/constants/theme';
+import { verticalScale } from '@/utils/styling';
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
 
 const Welcome = () => {
   return (
     <ScreenWrapper showPattern={true}>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <View style={{ alignItems: 'center' }}>
+          <Typo color={colors.white} size={43} fontWeight={'900'}>
+            Bubbly
+          </Typo>
+        </View>
+        <Animated.Image
+          entering={FadeIn.duration(700).springify()}
+          source={require('../../assets/images/welcome.png')}
+          style={styles.welcomeImage}
+          resizeMode={'contain'}
+        />
+        <View>
+          <Typo color={colors.white} size={33} fontWeight={'800'}>
+            Stay Connected
+          </Typo>
+          <Typo color={colors.white} size={33} fontWeight={'800'}>
+            with your friends
+          </Typo>
+          <Typo color={colors.white} size={33} fontWeight={'800'}>
+            and family
+          </Typo>
+        </View>
+        {/* https://youtu.be/_Y9A7RVIpMQ?list=LL&t=1729 */}
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingHorizontal: spacingX._20,
+    marginVertical: spacingY._10,
+  },
+
+  background: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+  },
+  welcomeImage: {
+    height: verticalScale(300),
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+});
--- frontend/types.ts
+++ frontend/types.ts
@@ -1,4 +1,13 @@
-import { ViewStyle } from 'react-native';
+import { TextProps, TextStyle, ViewStyle } from 'react-native';
+
+export type TypoProps = {
+  size?: number;
+  color?: string;
+  fontWeight?: TextStyle['fontWeight'];
+  children: any | null;
+  style?: TextStyle;
+  textProps?: TextProps;
+};
 
 export type ScreenWrapperProps = {
   style?: ViewStyle;
